e9e95553b66d29e6fff0b7d823db3eb4f118320b
phamchivy <chivy20033012@gmail.com>
2024-08-25 21:13:43 +0700

    Thêm thư mục mới vào repository

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -10,7 +10,7 @@ import {
   } from "@google/generative-ai"
   
   const MODEL_NAME = "gemini-1.5-pro-latest";
-  const API_KEY = "";
+  const API_KEY = import.meta.env.VITE_API_KEY;
   
   async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
